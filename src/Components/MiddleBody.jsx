4c3177704ca55d750498d1539994e111e5722f50
Andres Villarroel <andres121231@gmail.com>
2025-02-14 20:27:30 -0500

    Added image comparison slider for home page. Replaced the dots for the service list with check marks.

--- src/Components/MiddleBody.jsx
+++ src/Components/MiddleBody.jsx
@@ -2,17 +2,27 @@
 import React from 'react'
 import ServiceList from './ServiceList'
 import '/src/CSS/MiddleBody.css'
+import ReactCompareImage from 'react-compare-image'
+import beforeImg from '/src/assets/images/before.jpeg'
+import afterImg from '/src/assets/images/after.jpeg'
 
 export default function MiddleBody() {
   return (
     <>
         <div className='MainContainer_MiddleBody'>
             <div className='AboutMeContainer'>
-              <h2 className='AboutMeTitleHeader'>Villarroel Remodeling</h2>
-              <p className='AboutMeParagraph'>With 20 years of hands-on experience, I’ve tackled everything from replacing water heaters to remodeling entire kitchens. 
-                  I take pride in delivering quality craftsmanship, reliable service, and attention to detail on every job, big or small. 
-                  Whether you need minor repairs or a full renovation, I bring the skills and expertise to get it done right.
-              </p>
+              <div className='AboutMePositionContainer'>
+                <div className='ImageCompare'>
+                  <ReactCompareImage leftImage={beforeImg} rightImage={afterImg}/>
+                </div>
+                <div className='Header_paragraph'>
+                  <h2 className='AboutMeTitleHeader'>Villarroel Remodeling</h2>
+                  <p className='AboutMeParagraph'>With 20 years of hands-on experience, I’ve tackled everything from replacing water heaters to remodeling entire kitchens. 
+                      I take pride in delivering quality craftsmanship, reliable service, and attention to detail on every job, big or small. 
+                      Whether you need minor repairs or a full renovation, I bring the skills and expertise to get it done right.
+                  </p>
+                </div>
+              </div>
             </div>
             <ServiceList />
         </div>
