735ef4f74c9d940a9622221bc077c32f2e327462
Andres Villarroel <andres121231@gmail.com>
2025-02-27 11:13:01 -0500

    Fixed bug where app was not taking up the whole width of the screen on the home page.

--- src/Components/UpperBody.jsx
+++ src/Components/UpperBody.jsx
@@ -7,17 +7,17 @@ import HomePageLinkButton from './HomePageLinkButton'
 function UpperBody() {
   return (
     <div className='ImageContainer'>
-        <div className='GradientContainer'>
-          <div className='logoContainer'>
-            <img src={LogoImage}/>
-          </div>
-            <div className='ButtonContainer_UpperBody'>
-                <HomePageLinkButton Name='About' />
-                <HomePageLinkButton Name='Services' />
-                <HomePageLinkButton Name='Gallery' />
-                <HomePageLinkButton Name='Contact' />
-            </div>
+      <div className='GradientContainer'>
+        <div className='logoContainer'>
+          <img src={LogoImage}/>
         </div>
+        <div className='ButtonContainer_UpperBody'>
+            <HomePageLinkButton Name='About' />
+            <HomePageLinkButton Name='Services' />
+            <HomePageLinkButton Name='Gallery' />
+            <HomePageLinkButton Name='Contact' />
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,6 @@
-import testImage from '/src/assets/images/testImageHome.jpg';
 import "/src/CSS/Home.css";
 import UpperBody from '../Components/UpperBody';
 import MiddleBody from '../Components/MiddleBody';
-import Footer from '../Components/Footer';
 
 export default function Home(){
     //<img src={testImage} alt="TestingImage"/>
